073b4c0bf7c2741f11ce02c790d71c3e2c0d51e5
haosdent <haosdent@gmail.com>
2014-11-20 13:28:09 +0800

    Add ast.

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -65,6 +65,58 @@ static int gettok() {
     return ThisChar;
 }
 
+namespace {
+    class ExprAST {
+    public:
+        virtual ~ExprAST() {};
+    };
+
+    class NumberExprAST : public ExprAST {
+    public:
+        NumberExprAST(double val) {};
+    };
+
+    class VariableExprAST : public ExprAST {
+        std::string Name;
+    public:
+        VariableExprAST(const std::string &name) : Name(name) {}
+    };
+
+    class BinaryExprAST : public ExprAST {
+    public:
+        BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs) {}
+    };
+
+    class CallExprAST : public ExprAST {
+        std::string Callee;
+        std::vector<ExprAST*> Args;
+    public:
+        CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+        : Callee(callee), Args(args) {}
+    };
+
+    class PrototypeAST {
+        std::string Name;
+        std::vector<std::string> Args;
+    public:
+        PrototypeAST(const std::string &name, const std::vector<std::string> & args): Name(name), Args(args) {}
+    };
+
+    class FunctionAST {
+    public:
+        FunctionAST(PrototypeAST *proto, ExprAST *body) {}
+    };
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
