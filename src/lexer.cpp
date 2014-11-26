1bcd642b7d7fcf7886ddbf96582a268b3a9a0b2b
haosdent <haosdent@gmail.com>
2014-11-26 16:49:26 +0800

    Add ParsePrimary function

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -187,6 +187,17 @@ static ExprAST *ParseParenExpr() {
     return V;
 }
 
+static ExprAST *ParsePrimary() {
+    switch (CurTok) {
+        default: return Error("unknown token when expecting an expression");
+        case tok_identifier: return ParseIdentifierExpr();
+        case tok_number: return ParseNumberExpr();
+        case '(': return ParseParenExpr();
+    }
+}
+
+
+
 static void HandleDefinition() {
     if (ParseDefinition()) {
         fprintf(stderr, "Parsed a function definition.\n");
